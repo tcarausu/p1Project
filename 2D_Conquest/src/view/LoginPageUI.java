9f5f00ba18b34a226b51b10b8305f4c4b6a380c7
Toader Carausu <tcarausu@gmail.com>
2018-11-07 19:18:59 +0100

    Toader (#5)
    * ignore me!!
    
    * firstly login reconfig

--- 2D_Conquest/src/view/LoginPageUI.java
+++ 2D_Conquest/src/view/LoginPageUI.java
@@ -1,37 +1,43 @@
 package view;
 
-import notNeededRighNow.newframe;
-
 import javax.swing.*;
 
 public class LoginPageUI extends JFrame {
+    //        private MyController myController;
+    private JButton blogin = new JButton("Login");
+    private JTextField txuser = new JTextField(15);
+    private JPasswordField pass = new JPasswordField(15);
 
-    public static void main(String[] args) {
-        LoginPageUI frameTabel = new LoginPageUI();
-    }
+    public LoginPageUI(
+//                MyController myController
+    ) {
 
+        super("LoginPage UI");
 
-    public LoginPageUI() {
-        JButton blogin = new JButton("Login");
-        JPanel panel = new JPanel();
-        JTextField txuser = new JTextField(15);
-        JPasswordField pass = new JPasswordField(15);
+//            this.myController=myController;
+        setLayout(null);
+        setResizable(false);
+        setVisible(true);
+        this.setDefaultCloseOperation(DISPOSE_ON_CLOSE);
 
         setTitle("Login Authentication");
+//            introduceLogin();
+//
+//        }
+//
+//        private void introduceLogin() {
         setSize(300, 200);
         setLocation(500, 280);
-        panel.setLayout(null);
 
 
         txuser.setBounds(70, 30, 150, 20);
         pass.setBounds(70, 65, 150, 20);
         blogin.setBounds(110, 100, 80, 20);
 
-        panel.add(blogin);
-        panel.add(txuser);
-        panel.add(pass);
+        super.add(blogin);
+        super.add(txuser);
+        super.add(pass);
 
-        getContentPane().add(panel);
         setDefaultCloseOperation(EXIT_ON_CLOSE);
         setVisible(true);
         blogin.addActionListener(ae -> {
@@ -39,10 +45,10 @@ public class LoginPageUI extends JFrame {
             String ppaswd = pass.getText();
             if (puname.equals("test") && ppaswd.equals("12345")) {
                 CountryUI countryUI = new CountryUI();
+//                if (myController.jdbcConnectForLogin(puname, ppaswd)) {
                 countryUI.setVisible(true);
                 dispose();
             } else {
-
                 JOptionPane.showMessageDialog(null, "Wrong Password / Username");
                 txuser.setText("");
                 pass.setText("");
