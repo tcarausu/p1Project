56ac673c8e3d18e108c2a5911dc791705109bdb4
uezenmud <44086734+uezenmud@users.noreply.github.com>
2018-12-10 16:23:01 +0100

    thread + timer

--- 2D_Conquest/src/view/difficulty/EasyQuestionUI.java
+++ 2D_Conquest/src/view/difficulty/EasyQuestionUI.java
@@ -33,6 +33,19 @@ public class EasyQuestionUI extends JFrame {
     private JLabel timeSpentOnTheQuiz = new JLabel();
     private JLabel totalNrOfQuestions = new JLabel();
 
+    private static int milliseconds = 0;
+    private static int seconds = 0;
+    private static int minutes = 0;
+    private static int hours = 0;
+
+    private static boolean state = true;
+
+    private JLabel hour = new JLabel("00: ");
+    private JLabel minute = new JLabel("00: ");
+    private JLabel second = new JLabel("00: ");
+    private JLabel millisecond = new JLabel("00");
+
+    private  Thread t = new Thread();
 
     /**
      * Easy Question UI's Constructor
@@ -75,6 +88,16 @@ public class EasyQuestionUI extends JFrame {
         super.add(radioButton3);
         super.add(radioButton4);
 
+        super.add(hour);
+        super.add(minute);
+        super.add(second);
+        super.add(millisecond);
+
+        hour.setBounds(10, 10, 30, 30);
+        minute.setBounds(40, 10, 30, 30);
+        second.setBounds(70, 10, 30, 30);
+        millisecond.setBounds(110, 10, 30, 30);
+
         setSize(750, 500);
         setLocation(500, 200);
 
@@ -92,6 +115,8 @@ public class EasyQuestionUI extends JFrame {
         radioButton2.setBounds(360, 250, 150, 50);
         radioButton3.setBounds(150, 200, 150, 50);
         radioButton4.setBounds(360, 200, 150, 50);
+
+        startTimer();
 
         question.setText(controller.questionToBeAnswered(difficultyLevel, region));
 
@@ -157,7 +182,7 @@ public class EasyQuestionUI extends JFrame {
                     try {
                         int value = nrOfCurrentQAnswered.getAndIncrement();
                         int timeSpentOnAQuestion = timeSpent.getAndIncrement();
-                        controller.skipToNextQuestion(value, difficultyLevel,timeSpentOnAQuestion);
+                        controller.skipToNextQuestion(value, difficultyLevel, timeSpentOnAQuestion);
                         if (value >= 20) {
                             dispose();
                             controller.openScoreWindow();
@@ -175,6 +200,7 @@ public class EasyQuestionUI extends JFrame {
         );
         done.addActionListener(e -> {
             dispose();
+            stopTimer();
             controller.openScoreWindow();
         });
 
@@ -182,7 +208,7 @@ public class EasyQuestionUI extends JFrame {
     }
 
     private void validationOfRButton(String answer, int value, int timeSpentOnAQuestion) throws SQLException {
-        controller.updateScoreOnForUserAndDifficulty(answer, value, difficultyLevel,timeSpentOnAQuestion);
+        controller.updateScoreOnForUserAndDifficulty(answer, value, difficultyLevel, timeSpentOnAQuestion);
         if (value >= 20) {
             dispose();
             controller.openScoreWindowOnUser(controller.getUser().getUserName());
@@ -192,5 +218,63 @@ public class EasyQuestionUI extends JFrame {
             controller.openEasyWindow(region);
 
         }
+    }
+
+    private void stopTimer() {
+        t.stop();
+        done.addActionListener(e -> {
+            state = false;
+            System.out.printf("You finished in this time: %d h:%d m:%d s", hours, minutes, seconds);
+            System.out.println();
+            hours = 0;
+            minutes = 0;
+            seconds = 0;
+            milliseconds = 0;
+        });
+
+    }
+
+    private void startTimer() {
+
+        state = true;
+
+        t= new Thread(() -> {
+            for (; ; )
+                if (state) {
+                    try {
+                        Thread.sleep(1);
+
+                        if (milliseconds > 762) {
+                            milliseconds = 0;
+                            seconds++;
+                        }
+                        if (seconds > 60) {
+                            milliseconds = 0;
+                            seconds = 0;
+                            minutes++;
+                        }
+                        if (minutes > 60) {
+                            milliseconds = 0;
+                            seconds = 0;
+                            minutes = 0;
+                            hours++;
+                        }
+                        millisecond.setText(" : " + milliseconds);
+
+                        milliseconds++;
+                        second.setText(" : " + seconds);
+                        minute.setText(" : " + minutes);
+                        hour.setText("" + hours);
+                    } catch (Exception ignored) {
+
+                    }
+
+                } else {
+                    break;
+                }
+
+        });
+        t.start();
+
     }
 }
